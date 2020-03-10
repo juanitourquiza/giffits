b0d8aceb57289c1e44a6f19ecdb67ab9e2f21e59
JuanUrquiza <macbookpro@iMacJuanUrquiza.local>
2020-03-10 18:45:34 -0500

    se carga app en vue

---
+++ front/src/components/Panel.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-card>
+        <v-toolbar flat color="transparent">
+            <v-toolbar-title>{{titulo}}</v-toolbar-title>
+        </v-toolbar>
+        <v-divider/>
+        <v-card-text>
+            <slot></slot> 
+        </v-card-text>       
+    </v-card>
+</template>
+
+<script>
+export default {
+    props:['titulo']
+}
+</script>
+
+<style>
+
+</style>
